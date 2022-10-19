9ad12ada31e858f14eefaf98c47b1fc14bad5533
Marcos Vinnicius Martins <gronytzki@gmail.com>
2022-10-19 20:18:21 -0300

    Aula 3, códigos feitos em aula, dia 19/10/22

--- Aula3--Revisao_I/Aula/scripts/aula3_script.js
+++ Aula3--Revisao_I/Aula/scripts/aula3_script.js
@@ -1,13 +1,64 @@
 // @@@ CICLOS (Percorrendo, fazendo iterações em dados)
+export function trabalhandoComArrays() {
 
-// FOR IN
+    // FOR IN
+    let aula = {
+        titulo: "Aula 3 - revisão",
+        horario: "19H até 20:45",
+        tema: {
+            titulo: "Aula de revisão",
+            bimestre: "3Bi"
+        }
+    }
+    alert(aula.tema.titulo)
+    for (let propriedade in aula) {
+        console.log(aula[propriedade]);
+    }
 
-//Laço For simples
+    //Criando um array
+    let arrayAlunos = ["Gilson", "Thiago", "Luiz fernando"];
+    arrayAlunos.push("Cristian");
+    arrayAlunos.push("Juliana");
 
-//FOR OF
+    console.log(arrayAlunos[3]); //Buscando um elemento específico
 
-//ForEach
+    //Laço For simples
+    for (let i = 0; i < arrayAlunos.length; i++) {
+        console.log(arrayAlunos[i]);
+    }
 
-//Map()
+    //FOR OF
+    console.log("\n");
+    for (let elemento of arrayAlunos) {
+        console.log(elemento);
+    }
 
-// @@ Importando e exportando scripts
+    //ForEach
+    let arrayAlunos.forEach(aluno => {
+        return aluno.toUpperCase();
+    });
+
+    /* function exibeNomeAluno(alunoRecebido) {
+        console.log(alunoRecebido);
+    } */
+
+
+    //Map
+    let novaArray = arrayAlunos.map(aluno => {
+        return aluno.toUpperCase();
+    });
+
+    console.log(novaArray);
+
+    let vitoria = false;
+    while (!vitoria) { //true
+        //Realiza a ação...
+        vitoria = true;
+}
+
+    //trabalhandoComArrays();
+    export function exibeNome() {
+    console.log("O nome é Marcos");
+}
+
+export default trabalhandoComArrays;
---
+++ Aula3--Revisao_I/Aula/scripts/aula3_script2.js
@@ -0,0 +1,6 @@
+/* Importando funções de outro script do mesmo projeto */
+import {trabalhandoComArrays as trabalhandoArrays, exibeNome as nome} from './aula3_script.js';
+
+/* Funções importadas */
+trabalhandoArrays();
+nome();
