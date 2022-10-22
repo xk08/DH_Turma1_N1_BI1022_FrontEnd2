bd08da7a2e5a7846e0022624df2f85c0d5e64d28
Marcos Vinnicius Martins <gronytzki@gmail.com>
2022-10-22 20:41:20 -0300

    Aula 4, segunda semana front end 2 - modelos para a aula

---
+++ Aula4--DOM_IntroducaoAoDom/Aula/scripts/aula4_script.js
@@ -0,0 +1,3 @@
+/* NESTA AULA, FAREMOS APENAS A CAPTURA DAS INFORMAÇÕES DO HTML (SEM MODIFICAR OU ADICIONAR E REMOVER ELEMENTOS) 
+-> Nas proximas aulas, faremos a alteração de alguns elementos e de suas propriedades*/
+
