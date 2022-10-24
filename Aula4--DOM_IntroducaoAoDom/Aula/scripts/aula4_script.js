bf9c6cbf3131086ee15d32d0e8f534bde754c501
Marcos Vinnicius Martins <gronytzki@gmail.com>
2022-10-24 20:17:38 -0300

    Aula 4, códigos feitos em aula, dia 24/10

--- Aula4--DOM_IntroducaoAoDom/Aula/scripts/aula4_script.js
+++ Aula4--DOM_IntroducaoAoDom/Aula/scripts/aula4_script.js
@@ -1,3 +1,22 @@
 /* NESTA AULA, FAREMOS APENAS A CAPTURA DAS INFORMAÇÕES DO HTML (SEM MODIFICAR OU ADICIONAR E REMOVER ELEMENTOS) 
 -> Nas proximas aulas, faremos a alteração de alguns elementos e de suas propriedades*/
 
+console.log(window.document);
+
+console.log(document.querySelector('#item1').innerText);
+
+console.log(document.getElementById('item1').innerText);
+
+let articles = document.querySelectorAll("article");
+console.log(articles[2]);
+
+let elementoSelecionado;
+for (let elememento of articles ) {
+console.log(elememento);
+
+    if(elememento.id == "card1") {
+        elementoSelecionado = elememento;
+    }
+}
+
+console.log(elementoSelecionado);
