f5ba01e23a4586a9336ae9199f21b17475508c8b
Marcos Vinnicius Martins <gronytzki@gmail.com>
2022-10-26 18:40:35 -0300

    Aula 6, arquivos base

---
+++ Aula6--DOM_TrabalhandoComNos/Aula/scripts/script_aula6.js
@@ -0,0 +1,7 @@
+//##### INTERAÇÃO COM A LISTA DE FELINOS
+
+//Remoção de um ou mais elementos da lista
+
+//Inserindo um novo card na list de elementos
+
+//Inserindo em uma posição específica da lista
