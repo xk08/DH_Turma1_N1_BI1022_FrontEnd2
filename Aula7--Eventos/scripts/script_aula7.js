bb37a926a769ec3db61b2ce7cf354e24ebdcc415
Marcos Vinnicius Martins <gronytzki@gmail.com>
2022-10-31 20:07:34 -0300

    Aula 7 - Eventos, arquivos feitos em aula - Dia 31/10/22

--- Aula7--Eventos/scripts/script_aula7.js
+++ Aula7--Eventos/scripts/script_aula7.js
@@ -1,5 +1,13 @@
 /* 1° Bloco da aula: Atributos dinâmicos */
 
+let textNome = document.getElementById("textNome");
+textNome.setAttribute("class", "nova-classe");
+textNome.setAttribute("outro", "nova-classe");
+console.log(textNome);
+
+let img = document.createElement("img");
+img.setAttribute("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/800px-Unofficial_JavaScript_logo_2.svg.png")
+document.body.appendChild(img);
 
 /* 2° Bloco da aula: Eventos */
 /*
@@ -12,14 +20,45 @@ Principais tipos de eventos que temos:
 
 ///@@@@ Eventos de janela/pagina
 
+onload = () => {
+    boasvindas();
+};
+
+function boasvindas() {
+    console.log("Clicou no botão");
+    alert("Seja bem-vindo usuário")
+}
 
 ///@@@@ Eventos de formulários
+
+let btn = document.getElementById("botaoEnviar");
+console.log(btn);
 
 
 ///@@@@ Eventos do mouse (onClick)
 
+btn.ondblclick = function(){
+    boasvindas();
+}
+
+btn.addEventListener("click", function () {
+    boasvindas();
+
+});
+
+function exibeIdadeUsuario() {
+    let inputIdade = document.getElementById("textIdade");
+    console.log(inputIdade.value);
+}
 
 ///@@@@ Eventos do mouse, além do click (onClick)
 
+btn.onmouseover = function() {
+    console.log("Esta em cima do botão de enviar");
+    btn.style.width = "50%"
+}
 
-///@@@@@ Eventos de teclado
+btn.onmouseout = function() {
+    console.log("Saiu do botão de enviar");
+    btn.style.width = "10%"
+}
