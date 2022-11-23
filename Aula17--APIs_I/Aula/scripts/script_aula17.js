35fa3806d2a9f2b9b3940e5692d31d6c747dd2a7
Marcos Vinnicius Martins <gronytzki@gmail.com>
2022-11-23 20:29:49 -0300

    Aula 17, codigos feitos em aula, dia 23/11/22

--- Aula17--APIs_I/Aula/scripts/script_aula17.js
+++ Aula17--APIs_I/Aula/scripts/script_aula17.js
@@ -4,15 +4,35 @@ let exibeResultado = document.getElementById('exibeResultado');
 
 botaoPesquisar.addEventListener('click', function (evento) {
     evento.preventDefault();
-
     if (quantidadeFormulario.value != "") {
-        //Executar a busca a API...
-
-        
+        //GET
+        fetch(`https://dog.ceo/api/breedsss/image/random/${quantidadeFormulario.value}`)
+            .then(
+                function (resultado) {
+                    console.log(resultado);
+                    return resultado.json()
+                }
+            ).then(
+                function (resultado) {
+                    renderizaImgCachorros(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                }
+            );
     } else {
         alert("Você deve informar o número de imagens que deseja buscar")
     }
 });
+
+function renderizaImgCachorros(listaCachorros) {
+    for (let cachorro of listaCachorros.message) {
+        let img = document.createElement("img");
+        img.setAttribute("src", cachorro);
+        exibeResultado.appendChild(img);
+    }
+}
 
 function atualizaPagina() {
     window.location.reload();
